6babf8d007026f82b90cc3581bca6abee02c26b3
ironmarcus <ironshawn@gmail.com>
2023-12-24 16:42:47 -0500

    Lesson 13: Functions as props

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,9 +1,11 @@
 
 const BlogList = (props) => {
-
+    // It appears when I include blogs, title, handleDelete above, it does not work
+    const BlogList = (props)
     const blogs = props.blogs;
-    console.log(props, blogs)
+    // console.log(props, blogs)
     const title = props.title;
+    const handleDelete=props.handleDelete;
     return ( 
         <div className="blog-list">
             <h2>{title}</h2>
@@ -11,6 +13,7 @@ const BlogList = (props) => {
                 <div className="blog-preview" key={blog.id}>
                     <h2>{blog.title}</h2>
                     <p> Writte by {blog.author}</p>
+                    <button onClick={() => handleDelete(blog.id)}>Delete Blog</button>
                 </div>
             ))}
         </div>
