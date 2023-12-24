0ec1da83cfde62d96885680979f87b2b39c58d9c
ironmarcus <ironshawn@gmail.com>
2023-12-24 17:20:14 -0500

    Lesson 19: handling fetch errors

--- src/Home.js
+++ src/Home.js
@@ -3,25 +3,36 @@ import BlogList from './BlogList';
 
 const Home = () => {
     const[ blogs, setBlogs] = useState(null);
-
-    const [name, setName] = useState('mario');
+    const [isPending, setIsPending] = useState(true);
+    const [error, setError] = useState(null);
 
 
     useEffect(() => {
         fetch('http://localhost:8000/blogs')
-            .then(res => {
-                return res.json()
-            })
-            .then(data => {
-                console.log(data);
-                setBlogs(data);
-            })
+                .then(res => {
+                    console.log(res);
+                    if(!res.ok){
+                        throw Error('Could not fetch the data for that resource');
+                    }
+                    return res.json()
+                })
+                .then(data => {
+                    console.log(data);
+                    setBlogs(data);
+                    setIsPending(false);
+                    setError(null);
+                })
+                .catch(err => {
+                    setIsPending(false);
+                   // console.log(err.message);
+                   setError(err.message);
+                })
     }, [])
     return (  
         <div className="home">
+            { error && <div> { error } </div>}
+            { isPending && <div>Loading...</div>}
             {blogs && <BlogList blogs={blogs}  title="All Blogs"/>}
-            <button onClick={() => setName('luigi')}>change name</button>
-            <p>{ name }</p>
         </div>
     );
 }
