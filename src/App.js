999bf0531a96f363cf33f81ccd6b65e061310f19
ironmarcus <ironshawn@gmail.com>
2023-12-24 22:51:38 -0500

    Lesson 21: React-Router. Since I installed a higher version of the react-router-dom, I had to do use the switch and use a few things differently.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import Navbar from './Navbar';
 import Home from './Home';
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 
 function App() {
   const title = 'Welcome to the new blog';
   const likes = 50;
   return (
-    <div className="App">
-      <Navbar/>
-      <div className="content">
-        <Home />
-        <h1>{title}</h1>
-        <p> Liked {likes} times</p>
+    <Router>
+      <div className="App">
+        <Navbar/>
+        <div className="content">
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
